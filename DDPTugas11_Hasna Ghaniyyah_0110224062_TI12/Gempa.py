3c9f1b747415ff286c58777b8b529f831b90ec25
Hasna1606 <hasnaghaniyyah97@gmail.com>
2024-12-06 10:31:49 +0700

    Tugas Pertemuan 11

---
+++ DDPTugas11_Hasna Ghaniyyah_0110224062_TI12/Gempa.py
@@ -0,0 +1,25 @@
+class Gempa:
+    #konstruktor Inisialisasi atribut lokasi dan skala
+    def __init__(self, lokasi, skala,):
+
+        #atribut
+        self.lokasi = lokasi
+        self.skala = skala
+
+    #method menentukan dampak skala gempa
+    def dampak(self):
+
+        #statement/logika
+        if self.skala < 2:
+            print("Dampak Gempa Tidak Berasa")
+        elif self.skala >= 2 and self.skala <= 4:
+            print("Dampak Gempa Bangunan Retak-retak")
+        elif self.skala > 4 and self.skala <= 6:
+            print("Dampak Gempa Bangunan Roboh")
+        elif self.skala > 6:
+            print("Dampak Gempa Bangunan Roboh dan Berpotensi Tsunami")
+        
+
+        #Menampilkan lokasi dan skala
+        print(f"Lokasi Gempa: {self.lokasi}")
+        print(f"Skala Gempa: {self.skala}")
